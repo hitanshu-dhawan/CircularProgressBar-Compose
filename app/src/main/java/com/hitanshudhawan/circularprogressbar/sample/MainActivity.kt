7a6fc1fa6399aed885c15d8a516b61382f208579
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2021-07-14 22:24:15 +0530

    :sparkles: Introduced CircularProgressBar composable

--- app/src/main/java/com/hitanshudhawan/circularprogressbar/sample/MainActivity.kt
+++ app/src/main/java/com/hitanshudhawan/circularprogressbar/sample/MainActivity.kt
@@ -3,7 +3,12 @@ package com.hitanshudhawan.circularprogressbar.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.hitanshudhawan.circularprogressbar.CircularProgressBar
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +16,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                CircularProgressBar()
+                Row(Modifier.background(Color.Red)) {
+                    CircularProgressBar(
+                        modifier = Modifier.weight(1f),
+                        progress = 0.25f,
+                        progressMax = 1.00f,
+                        progressBarColor = Color.Blue,
+                        progressBarWidth = 32.dp,
+                        backgroundProgressBarColor = Color.White,
+                        backgroundProgressBarWidth = 16.dp,
+                        roundBorder = true,
+                        startAngle = 90f
+                    )
+                    CircularProgressBar(
+                        modifier = Modifier.weight(1f),
+                        progress = 0.25f,
+                        progressMax = 1.00f,
+                        progressBarColor = Color.Blue,
+                        progressBarWidth = 16.dp,
+                        backgroundProgressBarColor = Color.White,
+                        backgroundProgressBarWidth = 32.dp,
+                        roundBorder = true,
+                        startAngle = 90f
+                    )
+                }
             }
         }
     }
