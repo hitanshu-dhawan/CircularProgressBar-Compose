03d42b3f0bf78c048674d6f21b4d26721fc4d399
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2021-07-22 09:31:50 +0530

    :wrench: Added maven publish plugin and its configuration

---
+++ buildSrc/src/main/java/com/hitanshudhawan/circularprogressbar/buildsrc/Versions.kt
@@ -0,0 +1,10 @@
+package com.hitanshudhawan.circularprogressbar.buildsrc
+
+object Versions {
+
+    object CircularProgressBarCompose {
+        const val versionCode = 1
+        const val versionName = "1.0.0-rc01"
+    }
+
+}
